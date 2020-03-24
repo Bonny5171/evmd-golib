3d9cae3f4543fd2f40b29a8b27b2e13a0a759bf4
leandro_alves_evr <leandro.alves@everymind.com.br>
2020-03-24 08:58:39 -0300

    Refatoração de query que busca o produto por ref1 e ref2

--- db/dao/product.go
+++ db/dao/product.go
@@ -6,8 +6,11 @@ import (
 	"bitbucket.org/everymind/evmd-golib/db"
 )
 
+//CheckProduct func returns if a product exists or not based on tenant_id, ref1 and ref2
 func CheckProduct(conn *sqlx.DB, tid int, ref1, ref2 string) (exists bool, err error) {
-	const query = "SELECT EXISTS(SELECT 1 FROM public.vw_produto_modelo_cor WHERE tenant_id = $1 AND trim(ref1) = $2 AND trim(ref2) = $3) AS \"exists\";"
+	// const query = "SELECT EXISTS(SELECT 1 FROM public.vw_produto_modelo_cor WHERE tenant_id = $1 AND trim(ref1) = $2 AND trim(ref2) = $3) AS \"exists\";"
+
+	const query = "SELECT EXISTS(SELECT 1 FROM public.resource_metadata WHERE tentant_id = $1 AND trim(ref1) = $2 AND trim(ref2) = $3) AS \"exists\";"
 
 	if err = conn.Get(&exists, query, tid, ref1, ref2); err != nil {
 		if err.Error() != "sql no results" {
