492953eea4c84d299535da054d187fe2d3842e58
leandro_alves_evr <leandro.alves@everymind.com.br>
2020-03-24 18:55:08 -0300

    Fix Insert Statement

--- db/dao/storagemetadata.go
+++ db/dao/storagemetadata.go
@@ -9,18 +9,19 @@ import (
 
 func SaveStorageMetadata(conn *sqlx.DB, data *model.StorageMetadata) (err error) {
 	query := `
-		INSERT INTO public.storage_metadata (tenant_id, product_code, color_code, sequence, size_type, content_type, "size", original_file_name, original_file_extension, is_active, is_deleted, last_modified) 
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, true, false, $12)
+		INSERT INTO public.storage_metadata (tenant_id, product_code, color_code, sequence, size_type, content_type, "size", original_file_name, original_file_extension, is_active, is_deleted, last_modified, md5) 
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, true, false, $10, $11)
 			ON CONFLICT (tenant_id, product_code, color_code, sequence, size_type)
 			DO UPDATE
 		   SET content_type			   = EXCLUDED.size_type,
 		   	   "size"                  = EXCLUDED."size",
 			   original_file_name      = EXCLUDED.original_file_name, 
 			   original_file_extension = EXCLUDED.original_file_extension,
-			   last_modified		   = EXCLUDED.last_modified
+			   last_modified		   = EXCLUDED.last_modified,
+			   md5					   = EXCLUDED.md5,
 			   updated_at              = now();`
 
-	_, err = conn.Exec(query, data.TenantID, data.ProductCode, data.ColorCode, data.Sequence, data.SizeType, data.ContentType, data.Size, data.OriginalFileName, data.OriginalFileExtension, data.LastModified)
+	_, err = conn.Exec(query, data.TenantID, data.ProductCode, data.ColorCode, data.Sequence, data.SizeType, data.ContentType, data.Size, data.OriginalFileName, data.OriginalFileExtension, data.LastModified, data.MD5)
 	if err != nil {
 		err = db.WrapError(err, "conn.Exec()")
 		return
