21acf4c3c397ae4931bb9713c2e914a1c621c201
leandro_alves_evr <leandro.alves@everymind.com.br>
2020-03-25 10:31:40 -0300

    Function to return products with no resource

--- db/dao/storagemetadata.go
+++ db/dao/storagemetadata.go
@@ -7,6 +7,7 @@ import (
 	"bitbucket.org/everymind/evmd-golib/db/model"
 )
 
+//SaveStorageMetadata func
 func SaveStorageMetadata(conn *sqlx.DB, data *model.StorageMetadata) (err error) {
 	query := `
 		INSERT INTO public.storage_metadata (tenant_id, product_code, color_code, sequence, size_type, content_type, "size", original_file_name, original_file_extension, is_active, is_deleted, last_modified, md5) 
@@ -25,6 +26,25 @@ func SaveStorageMetadata(conn *sqlx.DB, data *model.StorageMetadata) (err error)
 	if err != nil {
 		err = db.WrapError(err, "conn.Exec()")
 		return
+	}
+
+	return
+}
+
+//GetProductsWithNullB64 func
+func GetProductsWithNullB64(conn *sqlx.DB) (products []string, err error) {
+	query := `
+		SELECT 
+			LPAD(r.ref_1::text, 5, '0') || 
+			LPAD(r.ref_2::text, 5, '0') || 
+			LPAD(r."sequence"::text, 2, '0') || '_' || 
+			r.size_type::text AS product 
+			FROM tn_011.sfa_resource_metadata_product AS r 
+			WHERE r.full_content_b64 ISNULL;`
+
+	err = conn.Select(&products, query)
+	if err != nil {
+		return nil, db.WrapError(err, "conn.Select()")
 	}
 
 	return
