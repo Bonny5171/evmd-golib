dafb36bf03b018d1fa98a9db75d6548e7f653018
leandro_alves_evr <leandro.alves@everymind.com.br>
2020-05-19 12:55:11 -0300

    Tenant Struct updated

--- db/model/tenant.go
+++ db/model/tenant.go
@@ -8,19 +8,21 @@ import (
 )
 
 type Tenant struct {
-	ID               int            `db:"id"`
-	CompanyID        string         `db:"company_id"`
-	Name             string         `db:"name"`
-	OrgID            string         `db:"org_id"`
-	OrgType          string         `db:"organization_type"`
-	CustomDomain     string         `db:"custom_domain"`
-	IsSandbox        bool           `db:"is_sandbox"`
-	IsActive         bool           `db:"is_active"`
-	CreatedAt        time.Time      `db:"created_at"`
-	UpdatedAt        time.Time      `db:"updated_at"`
-	IsDeleted        bool           `db:"is_deleted"`
-	DeletedAt        pq.NullTime    `db:"deleted_at"`
-	LastModifiedByID sql.NullString `db:"last_modified_by_id"`
-	IsCloned         bool           `db:"is_cloned"`
-	SfClientID       string         `db:"sf_client_id"`
+	ID                 int            `db:"id"`
+	CompanyID          string         `db:"company_id"`
+	Name               string         `db:"name"`
+	OrgID              string         `db:"org_id"`
+	OrgType            string         `db:"organization_type"`
+	CustomDomain       string         `db:"custom_domain"`
+	IsSandbox          bool           `db:"is_sandbox"`
+	IsActive           bool           `db:"is_active"`
+	CreatedAt          time.Time      `db:"created_at"`
+	UpdatedAt          time.Time      `db:"updated_at"`
+	IsDeleted          bool           `db:"is_deleted"`
+	DeletedAt          pq.NullTime    `db:"deleted_at"`
+	LastModifiedByID   sql.NullString `db:"last_modified_by_id"`
+	IsCloned           bool           `db:"is_cloned"`
+	SfClientID         string         `db:"sf_client_id"`
+	SfClientSecret     string         `db:"sf_client_secret"`
+	SfCallbackTokenUrl string         `db:"sf_callback_token_url"`
 }
