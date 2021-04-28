4f6b8938630741377d9bf6dc3c568f17aa255dd2
Gustavo Lima <gustavolimash@gmail.com>
2021-04-27 22:20:13 -0300

    create new method specific to get view for config stack

--- db/dao/tenant.go
+++ db/dao/tenant.go
@@ -333,7 +333,22 @@ func GetTenantTablesToClone(conn *sqlx.DB, tenantID int) (t []model.TenantCloneT
 	  FROM itgr.vw_tenant_clone	 
 	 WHERE table_schema = 'tn_%03d';`, tenantID)
 
-	err = conn.Select(&t, query, tenantID)
+	err = conn.Select(&t, query)
+	if err != nil {
+		return nil, db.WrapError(err, "db.Conn.Select()")
+	}
+
+	return t, nil
+}
+
+// GetTenantTablesToCloneConfig function to return a list of tables to clone
+func GetTenantTablesToCloneConfig(conn *sqlx.DB, tenantID int) (t []model.TenantCloneTable, err error) {
+	query := fmt.Sprintf(`
+	SELECT table_schema, table_name
+	  FROM public.vw_tenant_clone	 
+	 WHERE table_schema = 'tn_%03d';`, tenantID)
+
+	err = conn.Select(&t, query)
 	if err != nil {
 		return nil, db.WrapError(err, "db.Conn.Select()")
 	}
